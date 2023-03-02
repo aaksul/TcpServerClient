8ea30ae01eaf83e56abdf3161043d28ba9190b96
aaksul <aksulperen@gmail.com>
2023-03-02 14:36:21 +0000

    Add test case that check connectOnSocket

--- test/tcpclient/tcpclienttest.hpp
+++ test/tcpclient/tcpclienttest.hpp
@@ -15,7 +15,7 @@ class TcpClientTest : public Test{
     public:
     TcpClient tcp_client{};
     FakeTcpSocket fake_tcp_socket{};
-    std::shared_ptr<TcpSocketI> ptr{&fake_tcp_socket,nulldeleter};
+    std::shared_ptr<TcpSocketClientI> ptr{&fake_tcp_socket,nulldeleter};
 };
 
 TEST_F(TcpClientTest,InitializaSocketTest){
@@ -23,12 +23,13 @@ TEST_F(TcpClientTest,InitializaSocketTest){
     tcp_client.InitializeSocket(ptr);
 }
 
-TEST_F(TcpClientTest,DISABLED_ConnectWithoutInitializeTest)
+TEST_F(TcpClientTest,ConnectWithoutInitializeTest)
 {
-    tcp_client.ip(0x7F000001);//network order
-    tcp_client.port(8080);
+    tcp_client.ip(0x0100007F);//network order
+    tcp_client.port(8007);
+    tcp_client.InitializeSocket(ptr);
     EXPECT_CALL(fake_tcp_socket,Connect(Le(0),_,_)).WillOnce(Return(20));
-    //tcp_client.Connect();
+    tcp_client.Connect(ptr);
 }
 
 
